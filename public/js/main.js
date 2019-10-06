af80de1a17fd62a55193c1f94e49e75c44f2b516
free3xm <51963408+free3xm@users.noreply.github.com>
2019-10-06 22:38:44 +0300

    up

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const headerText = document.querySelector(".text_wrapper"),
+        elements = document.querySelectorAll(".aboutme, .latestArticle_item, .gallery_item, .contact");
+        form = document.forms[0];
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    fetch("http://localhost:3000/",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:  JSON.stringify({
+        name: form.name.value,
+        email: form.name.value,
+        message: form.userMessage.value
+      })
+    }).then(function(response){
+      return  response.text();
+    })
+    .then(function(data){
+      form.name.value ="";
+      form.email.value ="";
+      form.userMessage.value ="";
+      form.btnSubmit.classList.add("success");
+      setTimeout(function(){
+        form.btnSubmit.classList.remove("success");
+      }, 2000);
+    }).catch(function(error){
+      form.btnSubmit.classList.add("error");
+      setTimeout(function(){
+        form.btnSubmit.classList.remove("error");
+      }, 2000);
+    });
+  });
+console.log( JSON.stringify({
+  name: form.name.value,
+  email: form.name.value,
+  message: form.userMessage.value
+}));
+  headerText.style.opacity = 1;
+  function onView(elems){
+  elems.forEach(e => window.scrollY + 100 > e.getBoundingClientRect().top ?
+   e.style.transform = "translate(0,0)" : false) ;
+  }
+  window.addEventListener("scroll", function(event){
+    onView(elements);
+    const layers = document.querySelectorAll(".header_paralax_layer"),
+          main = document.querySelector(".main");
+    let top = this.pageYOffset;
+    layers.forEach(e => {
+      let speed = e.getAttribute("data-speed"),
+          yPos = -(top * speed / 100);
+        e.style.transform = `translate3d(0px,${yPos}px, 0px)`;
+      })
+    });
+});
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express"),
+      app = express(),
+      path = require("path"),
+      bodyParser = require("body-parser"),
+      port = 3000,
+      nodemailer = require("nodemailer");
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.post("/", function(req, res){
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "free3test@gmail.com",
+      pass: "free30974836904"
+    }
+  });
+  transporter.sendMail({
+    from: "free3test@gmail.com",
+    to: "free3test@gmail.com",
+    subject: req.body.name,
+    text: `from: ${req.body.email}
+    message: ${req.body.massege}`
+  }, function(error){
+    console.log(error);
+  })
+  res.send("success");
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+     console.log(`server is listening on ${port}`)
+});
